48f85a86b8b8678cebc18ca870027b74215698cb
Anurag choudhury <idanurag567@gmail.com>
2025-12-04 16:16:45 +0530

    Lidar working as expected

--- rplidar_ros/src/rplidar_node.cpp
+++ rplidar_ros/src/rplidar_node.cpp
@@ -81,7 +81,7 @@ class RPlidarNode : public rclcpp::Node
         this->declare_parameter<std::string>("frame_id","laser_frame");
         this->declare_parameter<bool>("inverted", false);
         this->declare_parameter<bool>("angle_compensate", false);
-        this->declare_parameter<bool>("flip_x_axis", false);
+        this->declare_parameter<bool>("flip_x_axis", true);
         this->declare_parameter<bool>("auto_standby", false);
         this->declare_parameter<std::string>("topic_name",std::string("scan"));
         this->declare_parameter<std::string>("scan_mode",std::string());
@@ -97,12 +97,12 @@ class RPlidarNode : public rclcpp::Node
         this->get_parameter_or<std::string>("frame_id", frame_id, "laser_frame");
         this->get_parameter_or<bool>("inverted", inverted, false);
         this->get_parameter_or<bool>("angle_compensate", angle_compensate, false);
-        this->get_parameter_or<bool>("flip_x_axis", flip_x_axis, false);
+        this->get_parameter_or<bool>("flip_x_axis", flip_x_axis, true);
         this->get_parameter_or<bool>("auto_standby", auto_standby, false);
         this->get_parameter_or<std::string>("topic_name", topic_name, "scan");
         this->get_parameter_or<std::string>("scan_mode", scan_mode, std::string());
         if(channel_type == "udp")
-            this->get_parameter_or<float>("scan_frequency", scan_frequency, 20.0);
+            this->get_parameter_or<float>("scan_frequency", scan_frequency, 10.0);
         else
             this->get_parameter_or<float>("scan_frequency", scan_frequency, 10.0);
     }
@@ -464,9 +464,9 @@ public:
                 }
             }
 
-            start_scan_time = this->now();
+            start_scan_time = this->get_clock()->now();
             op_result = drv->grabScanDataHq(nodes, count);
-            end_scan_time = this->now();
+            end_scan_time = this->get_clock()->now();
             scan_duration = (end_scan_time - start_scan_time).seconds();
 
             if (op_result == SL_RESULT_OK) {
